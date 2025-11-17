004af595fb71c113707fac40e708e393f408c3f0
Mikolaj Gralczyk <mgralczyk@anaconda.com>
2025-11-17 12:30:19 +0100

    Update to 6.0.3

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 @echo on
 
 %PYTHON% -m pip install . --no-deps --no-build-isolation ^
-        --global-option="--with-libyaml" ^
-        --global-option="build_ext" ^
-        --global-option="-I%LIBRARY_INC%" ^
-        --global-option="-L%LIBRARY_LIB%"
+        --config-settings="--global-option=--with-libyaml" ^
+        --config-settings="--global-option=build_ext" ^
+        --config-settings="--global-option=-I%LIBRARY_INC%" ^
+        --config-settings="--global-option=-L%LIBRARY_LIB%"
